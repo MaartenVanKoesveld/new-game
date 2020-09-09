c293f13a81475674c32c8ea323c0a4199230c1d1
MaartenVanKoesveld <111147@emmauscollege.nl>
2020-09-09 14:50:10 +0000

    eerste lijn van breekbare blokjes toegevoegd

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-var balX = 50;
-var balY = 50;
+var balX = 200;
+var balY = 200;
 var richtingX = 5;
 var richtingY = 5;
-
-
-
-
-
-
-
-
-
+var array = new Array(15);
 
 
 /**
@@ -21,8 +13,10 @@ var richtingY = 5;
 function setup() {
   // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
   createCanvas(1280, 720);
-
-
+  // array maken voor blokjes
+  for(var i = 0; i < 21; i++){
+    array[i] = 1;
+  } 
 }
 
 
@@ -39,26 +33,43 @@ function draw() {
   fill(255, 0, 0);
 
   // teken een cirkel
-  ellipse(balX, balY, 80, 80);
+  ellipse(balX, balY, 30, 30);
     balX = balX + richtingX;
     balY = balY + richtingY;
 
 
   // teken platform
   rect(mouseX - 100, 650, 200, 20);
-// omkeren als tegen platform
-  if (balY == 650-40 && balX >= mouseX-100 && balX <= mouseX+100) {
+  // omkeren als tegen platform
+  if (balY == 650-15 && balX >= mouseX-100 && balX <= mouseX+100) {
       richtingY = richtingY * -1;
 
   }
-// omkeren als tegen zijkant
-  if (balX == 0 + 40 || balX == 1280 - 40){
+  // omkeren als tegen zijkant
+  if (balX == 0 + 15 || balX == 1280 - 15){
       richtingX = richtingX * -1;
   }
-// omkeren als tegen de bovenkant botst
-  if (balY == 0 + 40) {
+  // omkeren als tegen de bovenkant botst
+  if (balY == 0 + 15) {
       richtingY = richtingY * -1;
   }
+  // blokjes tekenen
+  for(var i = 0; i < 21; i++){
+      if(array[i] >0){
+        rect(1 + 61*i,1,59,30);
+        var data = array[i];
+      } 
+  }
+  // blokjes kunnen breken
+    if(balX > 0 && balX < 1280 && balY > 1 + 15 && balY < 30 + 15){
+      var arrayX = Math.floor(balX/61);
+      var data = array[arrayX];
+      if(data > 0){
+        array[arrayX] = data - 1;
+        richtingY *= -1;
+        
+      }
+    }
 
 
 
