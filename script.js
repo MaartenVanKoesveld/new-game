407562b33d33b2b2a96e448fa66d449dcb16cda9
MaartenVanKoesveld <111147@emmauscollege.nl>
2020-09-09 15:59:48 +0000

    5 rijen aan blokjes, blokHoogte en blokStraal toegevoegd, bugs gefixt.

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-var balX = 200;
-var balY = 200;
+var balX = 600;
+var balY = 500;
 var richtingX = 5;
 var richtingY = 5;
-var array = new Array(15);
+var array = new Array(21);
+var array2 = new Array(21);
+var array3 = new Array(21);
+var array4 = new Array(21);
+var array5 = new Array(21);
+var blokHoogte = 100;
+var balStraal = 15;
 
 
 /**
@@ -17,6 +23,18 @@ function setup() {
   for(var i = 0; i < 21; i++){
     array[i] = 1;
   } 
+  for(var i = 0; i < 21; i++){
+      array2[i] = 1;
+  }
+  for(var i = 0; i < 21; i++){
+      array3[i] = 1;
+  }
+  for(var i = 0; i < 21; i++){
+      array4[i] = 1;
+  }
+  for(var i = 0; i < 21; i++){
+      array5[i] = 1;
+  }
 }
 
 
@@ -33,43 +51,109 @@ function draw() {
   fill(255, 0, 0);
 
   // teken een cirkel
-  ellipse(balX, balY, 30, 30);
+  ellipse(balX, balY, 2*balStraal, 2*balStraal);
     balX = balX + richtingX;
     balY = balY + richtingY;
 
 
   // teken platform
-  rect(mouseX - 100, 650, 200, 20);
+  rect(balX - 100, 650, 200, 20);
   // omkeren als tegen platform
-  if (balY == 650-15 && balX >= mouseX-100 && balX <= mouseX+100) {
+  if (balY == 650 - balStraal && balX >= balX-100 && balX <= balX+100) {
       richtingY = richtingY * -1;
 
   }
   // omkeren als tegen zijkant
-  if (balX == 0 + 15 || balX == 1280 - 15){
+  if (balX <= 0 + balStraal || balX >= 1280 - balStraal){
       richtingX = richtingX * -1;
   }
   // omkeren als tegen de bovenkant botst
-  if (balY == 0 + 15) {
+  if (balY <= 0 + balStraal) {
       richtingY = richtingY * -1;
   }
   // blokjes tekenen
   for(var i = 0; i < 21; i++){
       if(array[i] >0){
-        rect(1 + 61*i,1,59,30);
+        rect(1 + 61 * i, blokHoogte, 59, 30);
         var data = array[i];
-      } 
-  }
-  // blokjes kunnen breken
-    if(balX > 0 && balX < 1280 && balY > 1 + 15 && balY < 30 + 15){
+      }
+    }
+
+  for(var i = 0; i < 21; i++){
+      if(array2[i] > 0){
+        rect(1 + 61 * i, blokHoogte + 30, 59, 30);
+        var data = array2[i]; 
+      }
+    } 
+
+  for(var i = 0; i < 21; i++){
+      if(array3[i] > 0){
+        rect(1 + 61 * i, blokHoogte + 60, 59, 30);
+        var data = array3[i]; 
+      }
+    } 
+
+  for(var i = 0; i < 21; i++){
+      if(array4[i] > 0){
+        rect(1 + 61 * i, blokHoogte + 90, 59, 30);
+        var data = array4[i]; 
+      }
+    } 
+
+  for(var i = 0; i < 21; i++){
+      if(array5[i] > 0){
+        rect(1 + 61 * i, blokHoogte + 120, 59, 30);
+        var data = array5[i]; 
+      }
+    } 
+
+
+  
+  // blokjes kunnen breken 
+    if(balX > 0 && balX < 1280 && balY > blokHoogte - balStraal && balY < blokHoogte + 30 + balStraal){
       var arrayX = Math.floor(balX/61);
       var data = array[arrayX];
       if(data > 0){
         array[arrayX] = data - 1;
         richtingY *= -1;
-        
       }
     }
+
+    if(balX > 0 && balX < 1280 && balY > blokHoogte + 30 - balStraal && balY < blokHoogte + 60 + balStraal){
+        var arrayX = Math.floor(balX/61);
+        var data = array2[arrayX];
+        if(data > 0){
+            array2[arrayX] = data -1;
+            richtingY *= -1;
+        }
+    } 
+
+    if(balX > 0 && balX < 1280 && balY > blokHoogte + 60 - balStraal && balY < blokHoogte + 90 + balStraal){
+        var arrayX = Math.floor(balX/61);
+        var data = array3[arrayX];
+        if(data > 0){
+            array3[arrayX] = data -1;
+            richtingY *= -1;
+        }
+    } 
+
+    if(balX > 0 && balX < 1280 && balY > blokHoogte + 90 - balStraal && balY < blokHoogte + 120 + balStraal){
+        var arrayX = Math.floor(balX/61);
+        var data = array4[arrayX];
+        if(data > 0){
+            array4[arrayX] = data -1;
+            richtingY *= -1;
+        }
+    } 
+
+    if(balX > 0 && balX < 1280 && balY > blokHoogte + 120 - balStraal && balY < blokHoogte + 150 + balStraal){
+        var arrayX = Math.floor(balX/61);
+        var data = array5[arrayX];
+        if(data > 0){
+            array5[arrayX] = data -1;
+            richtingY *= -1;
+        }
+    } 
 
 
 
