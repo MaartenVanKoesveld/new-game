18f506987693a2e14d0df7dd9b737e2f9ba3950d
MaartenVanKoesveld <111147@emmauscollege.nl>
2020-09-16 12:51:38 +0000

    class van bal en begin blokjes

--- script.js
+++ script.js
@@ -1,169 +1,81 @@
-var balX = 600;
-var balY = 500;
-var richtingX = 1;
-var richtingY = 1;
-var array = new Array(21);
-var array2 = new Array(21);
-var array3 = new Array(21);
-var array4 = new Array(21);
-var array5 = new Array(21);
-var blokHoogte = 100;
-var balStraal = 15;
-var plankX = mouseX;
-var px = Math.abs(balX - (mouseX - 100));
+class Bal {
+   constructor(myX, myY, mySpeedX, mySpeedY, mySuper) {
+      this.x = myX;
+      this.y = myY;
+      this.speedX = mySpeedX;
+      this.speedY = mySpeedY;
+      this.diameter = 80;
+      this.isSuper = mySuper;
+   }
 
-/**
- * setup
- * de code in deze functie wordt één keer uitgevoerd door
- * de p5 library, zodra het spel geladen is in de browser
- */
-function setup() {
-  // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
-  createCanvas(1280, 720);
-  // array maken voor blokjes
-  for(var i = 0; i < 21; i++){
-    array[i] = 1;
-  } 
-  for(var i = 0; i < 21; i++){
-      array2[i] = 1;
-  }
-  for(var i = 0; i < 21; i++){
-      array3[i] = 1;
-  }
-  for(var i = 0; i < 21; i++){
-      array4[i] = 1;
-  }
-  for(var i = 0; i < 21; i++){
-      array5[i] = 1;
-  }
+
+
+
+   show() {
+      if (this.isSuper === true) {
+         fill(255, 0, 0);
+      }
+      else {
+         fill(255, 100, 255);
+      }
+      
+      ellipse(this.x, this.y, this.diameter, this.diameter);
+   }
+
+   update() {
+      this.x = this.x + this.speedX;
+      this.y = this.y + this.speedY;
+
+      if (this.x <= 0 || this.x >= 1280 ) {
+         this.speedX = this.speedX * -1
+      }
+
+      if (this.y <= 0 || this.y >= 720) {
+         this.speedY = this.speedY * -1;
+      }
+   }
+}
+
+class Blok {
+    constructor(myBlokX, myBlokY) {
+    this.blokX = myBlokX;
+    this.blokY = myBlokY;
+    }
+
+    showBlok() {
+        rect(this.blokX, this.blokY, 128, 30);
+    }
+
 }
 
 
-/**
- * draw
- * de code in deze functie wordt meerdere keren per seconde
- * uitgevoerd door de p5 library, nadat de setup functie klaar is
- */
+
+
+var ballen = [new Bal(50, 100, 6, 3, false),
+              new Bal(300, 150, 5, 7, true)];
+
+
+
+
+function setup() {
+  // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
+  createCanvas(1280, 720);
+}
+ 
+
+
+
 function draw() {
   // Kleur de achtergrond blauw, zodat je het kunt zien
   background('blue');
-
-  textSize(30);
-  text(balX, 50, 100);
-  text(balY, 150, 100);
-  text(Math.sin(3.14 + (10+px) / 220 * 3.14) * 10, 300, 100);
-  text(Math.abs(balX - (mouseX - 100)), 400, 100);
-
-  // stel vulkleur in
-  fill(255, 0, 0);
-
-  // teken een cirkel
-  ellipse(balX, balY, 2*balStraal, 2*balStraal);
-    balX = balX + richtingX;
-    balY = balY + richtingY;
-
-
-  // teken platform
-  rect(mouseX - 100, 650, 200, 20);
-  // omkeren als tegen platform
-  if (balY >= 650 - balStraal && balY <= 670 - balStraal && balX >= mouseX-100 && balX <= mouseX+100) {
-      richtingY = Math.sin(3.14 + (10+px) / 220 * 3.14) * 10;
-      richtingX = Math.cos(3.14 - (10+px) / 220 * 3.14) * 10;
-
-  }
-  // omkeren als tegen zijkant
-  if (balX <= 0 + balStraal || balX >= 1280 - balStraal){
-      richtingX = richtingX * -1;
-  }
-  // omkeren als tegen de bovenkant botst
-  if (balY <= 0 + balStraal) {
-      richtingY = richtingY * -1;
-  }
-  // blokjes tekenen
-  for(var i = 0; i < 21; i++){
-      if(array[i] >0){
-        rect(1 + 61 * i, blokHoogte, 59, 30);
-        var data = array[i];
-      }
-    }
-
-  for(var i = 0; i < 21; i++){
-      if(array2[i] > 0){
-        rect(1 + 61 * i, blokHoogte + 30, 59, 30);
-        var data = array2[i]; 
-      }
-    } 
-
-  for(var i = 0; i < 21; i++){
-      if(array3[i] > 0){
-        rect(1 + 61 * i, blokHoogte + 60, 59, 30);
-        var data = array3[i]; 
-      }
-    } 
-
-  for(var i = 0; i < 21; i++){
-      if(array4[i] > 0){
-        rect(1 + 61 * i, blokHoogte + 90, 59, 30);
-        var data = array4[i]; 
-      }
-    } 
-
-  for(var i = 0; i < 21; i++){
-      if(array5[i] > 0){
-        rect(1 + 61 * i, blokHoogte + 120, 59, 30);
-        var data = array5[i]; 
-      }
-    } 
-
-
   
-  // blokjes kunnen breken 
-    if(balX > 0 && balX < 1280 && balY > blokHoogte - balStraal && balY < blokHoogte + 30 + balStraal){
-      var arrayX = Math.floor(balX/61);
-      var data = array[arrayX];
-      if(data > 0){
-        array[arrayX] = data - 1;
-        richtingY *= -1;
-      }
-    }
+  // ga alle ballen af
+  for (var i = 0; i < ballen.length; i++) {
+    var bal = ballen[i];
+    bal.show();
+    bal.update();
+  }
 
-    if(balX > 0 && balX < 1280 && balY > blokHoogte + 30 - balStraal && balY < blokHoogte + 60 + balStraal){
-        var arrayX = Math.floor(balX/61);
-        var data = array2[arrayX];
-        if(data > 0){
-            array2[arrayX] = data -1;
-            richtingY *= -1;
-        }
-    } 
-
-    if(balX > 0 && balX < 1280 && balY > blokHoogte + 60 - balStraal && balY < blokHoogte + 90 + balStraal){
-        var arrayX = Math.floor(balX/61);
-        var data = array3[arrayX];
-        if(data > 0){
-            array3[arrayX] = data -1;
-            richtingY *= -1;
-        }
-    } 
-
-    if(balX > 0 && balX < 1280 && balY > blokHoogte + 90 - balStraal && balY < blokHoogte + 120 + balStraal){
-        var arrayX = Math.floor(balX/61);
-        var data = array4[arrayX];
-        if(data > 0){
-            array4[arrayX] = data -1;
-            richtingY *= -1;
-        }
-    } 
-
-    if(balX > 0 && balX < 1280 && balY > blokHoogte + 120 - balStraal && balY < blokHoogte + 150 + balStraal){
-        var arrayX = Math.floor(balX/61);
-        var data = array5[arrayX];
-        if(data > 0){
-            array5[arrayX] = data -1;
-            richtingY *= -1;
-        }
-    } 
-
-
-
-
+rect(400, 400, 100, 25);
+  
 }
