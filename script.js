c5131abb9eda298f47efb6255f39554ab4d35c7f
MaartenVanKoesveld <111147@emmauscollege.nl>
2020-09-30 10:20:45 +0000

    super-class SpelElement toegevoegd

--- script.js
+++ script.js
@@ -1,63 +1,79 @@
 
 
 
-class Bal {
-   constructor(myX, myY, mySpeedX, mySpeedY, mySuper) {
-      this.x = myX;
-      this.y = myY;
-      this.speedX = mySpeedX;
-      this.speedY = mySpeedY;
-      this.diameter = 50;
-      this.isSuper = mySuper;
-   }
+class SpelElement {
+    x;
+    y;
+    constructor(_x, _y) {
+        this.x = _x;
+        this.y = _y;
+    }
+}
 
-   show() {
-      if (this.isSuper === true) {
-         fill(255,215,0);
-      } else {
-         fill(255, 100, 255);
-      }
 
-      ellipse(this.x, this.y, this.diameter, this.diameter);
-   }//show
 
-   update() {
-      this.x = this.x + this.speedX;
-      this.y = this.y + this.speedY;
-    // ballen omkeren tegen muren
-      if (this.x <= 0 + 0.5*this.diameter || this.x >= 1280 - 0.5*this.diameter ) {
-         this.speedX = this.speedX * -1;
-      }
+class Bal extends SpelElement {
+    speedX;
+    speedY;
+    diameter;
+    isSuper;
+    constructor(_x, _y, _SpeedX, _SpeedY, _Super) {
+        super (_x, _y);
+        this.speedX = _SpeedX;
+        this.speedY = _SpeedY;
+        this.diameter = 50;
+        this.isSuper = _Super;
+    }
 
-      if (this.y <= 0 + 0.5*this.diameter || this.y >= 720 - 0.5*this.diameter) {
-         this.speedY = this.speedY * -1;
-      }
-      // ballen omkeren tegen plank
-      if (this.x > mouseX - 100 && this.x < mouseX + 100 && this.y > 600 - 0.5*this.diameter && this.y < 625 + 0.5*this.diameter) {
-            this.speedY = 0.08 * Math.abs(mouseX-this.x) - 10;
-            if (mouseX-this.x > 0){
-                this.speedX = -1 * Math.sqrt(100-(this.speedY*this.speedY));
-            }
-            if (mouseX-this.x < 0){
-                this.speedX = Math.sqrt(100-(this.speedY*this.speedY));
-            }
-      }
-   }//update 
+    show() {
+        if (this.isSuper === true) {
+            fill(255,215,0);
+        } else {
+            fill(255, 100, 255);
+        }
+
+        ellipse(this.x, this.y, this.diameter, this.diameter);
+    }//show
+
+    update() {
+        this.x = this.x + this.speedX;
+        this.y = this.y + this.speedY;
+        // ballen omkeren tegen muren
+        if (this.x <= 0 + 0.5*this.diameter || this.x >= 1280 - 0.5*this.diameter ) {
+            this.speedX = this.speedX * -1;
+        }
+
+        if (this.y <= 0 + 0.5*this.diameter || this.y >= 720 - 0.5*this.diameter) {
+            this.speedY = this.speedY * -1;
+        }
+        // ballen omkeren tegen plank
+        if (this.x > mouseX - 100 && this.x < mouseX + 100 && this.y > 600 - 0.5*this.diameter && this.y < 625 + 0.5*this.diameter) {
+                this.speedY = 0.08 * Math.abs(mouseX-this.x) - 10;
+                if (mouseX-this.x > 0){
+                    this.speedX = -1 * Math.sqrt(100-(this.speedY*this.speedY));
+                }
+                if (mouseX-this.x < 0){
+                    this.speedX = Math.sqrt(100-(this.speedY*this.speedY));
+                }
+        }
+    }//update 
 }//class Bal
 
-class Blok {
-    constructor(myBlokX, myBlokY, myBlokWidth, myBlockHeight, myBlockStatus) {
-    this.blokX = myBlokX;
-    this.blokY = myBlokY;
-    this.blokWidth = myBlokWidth;
-    this.blokHeight = myBlockHeight;
-    this.blokStatus = myBlockStatus;
+class Blok extends SpelElement {
+    blokWidth;
+    blokHeight;
+    blokStatus;
+    constructor(_x, _y, _blokWidth, _blockHeight, _blockStatus) {
+    super (_x, _y);
+    this.blokWidth = _blokWidth;
+    this.blokHeight = _blockHeight;
+    this.blokStatus = _blockStatus;
     }
 
     show() {
         fill(255, 0, 0);
         if (this.blokStatus > 0){
-            rect(this.blokX, this.blokY, this.blokWidth, this.blokHeight);
+            rect(this.x, this.y, this.blokWidth, this.blokHeight);
         }
     }//show
 
@@ -93,7 +109,7 @@ function draw() {
     var bal = ballen[i];
     bal.show();
     bal.update();
-    if (ballen[i].x > 0 && ballen[i].x < 1280 && ballen[i].y > blokken[0].blokY && ballen[i].y < blokken[blokken.length-1].blokY + 25 && blokken[Math.floor(ballen[i].x/128) + 10 * Math.floor((ballen[i].y-50)/25)].blokStatus === 1){
+    if (ballen[i].x > 0 && ballen[i].x < 1280 && ballen[i].y > blokken[0].y && ballen[i].y < blokken[blokken.length-1].y + 25 && blokken[Math.floor(ballen[i].x/128) + 10 * Math.floor((ballen[i].y-50)/25)].blokStatus === 1){
         blokken[Math.floor(ballen[i].x/128) + 10 * Math.floor((ballen[i].y-50)/25)].blokStatus = 0;
         if (ballen[i].isSuper === false) {
             ballen[i].speedY = ballen[i].speedY * -1;   
